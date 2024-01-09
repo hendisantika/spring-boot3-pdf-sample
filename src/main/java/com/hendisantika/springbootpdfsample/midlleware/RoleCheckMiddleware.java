641a9380e91afa9c1c4082657fb3c28f58117dbf
hendisantika <hendisantika@yahoo.co.id>
2024-01-09 08:30:40 +0700

    Add RoleCheckMiddleware🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/com/hendisantika/springbootpdfsample/midlleware/RoleCheckMiddleware.java
@@ -0,0 +1,23 @@
+package com.hendisantika.springbootpdfsample.midlleware;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-pdf-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/9/24
+ * Time: 08:30
+ * To change this template use File | Settings | File Templates.
+ */
+public class RoleCheckMiddleware extends Middleware {
+
+    public boolean check(String email, String password) {
+        if (email.equals("admin@example.com")) {
+            System.out.println("Hello, admin!");
+            return true;
+        }
+        System.out.println("Hello, user!");
+        return checkNext(email, password);
+    }
+}
