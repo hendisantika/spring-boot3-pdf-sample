89170af6bedd6ef5ae2dc9fbe74013fbe59363a2
hendisantika <hendisantika@yahoo.co.id>
2024-01-09 08:34:36 +0700

    Add CustomLocaleResolver🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/com/hendisantika/springbootpdfsample/config/CustomLocaleResolver.java
@@ -0,0 +1,25 @@
+package com.hendisantika.springbootpdfsample.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-pdf-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/9/24
+ * Time: 08:34
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class CustomLocaleResolver extends AcceptHeaderLocaleResolver implements WebMvcConfigurer {
+
+    List<Locale> LOCALES = Arrays.asList(new Locale("en"), new Locale("km"));
+}
