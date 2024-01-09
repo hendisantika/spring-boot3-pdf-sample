7286a0cc1b2b8a0401fd3987bbf9f61099bc3766
hendisantika <hendisantika@yahoo.co.id>
2024-01-09 08:36:44 +0700

    Add SwaggerConfig🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/com/hendisantika/springbootpdfsample/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.hendisantika.springbootpdfsample.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-pdf-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/9/24
+ * Time: 08:36
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    private static final String REDIRECT_URL = "/swagger-ui.html";
+
+    @Value("${spring.mvc.servlet.path}")
+    private String baseUrl;
+}
