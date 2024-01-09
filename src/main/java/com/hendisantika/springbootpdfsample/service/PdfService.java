0bf708c2b06e8af0c14df4ffe149b5c1db440455
hendisantika <hendisantika@yahoo.co.id>
2024-01-09 08:57:27 +0700

    Add writeText in PdfService 🫸🌀✏️📗🐧🐳⬆

--- src/main/java/com/hendisantika/springbootpdfsample/service/PdfService.java
+++ src/main/java/com/hendisantika/springbootpdfsample/service/PdfService.java
@@ -1,7 +1,17 @@
 package com.hendisantika.springbootpdfsample.service;
 
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.pdf.PdfWriter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,5 +26,16 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class PdfService {
+    public void writeText() throws FileNotFoundException, DocumentException {
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream("iTextHelloWorld.pdf"));
+
+        document.open();
+        Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
+        Chunk chunk = new Chunk("Hello World", font);
+
+        document.add(chunk);
+        document.close();
+    }
 
 }
