2a54701e4844b58731053254876f9ead7ff772e2
hendisantika <hendisantika@yahoo.co.id>
2024-01-09 08:30:18 +0700

    Add Middleware🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/com/hendisantika/springbootpdfsample/midlleware/Middleware.java
@@ -0,0 +1,43 @@
+package com.hendisantika.springbootpdfsample.midlleware;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-pdf-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/9/24
+ * Time: 08:30
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class Middleware {
+
+    private Middleware next;
+
+    /**
+     * Builds chains of middleware objects.
+     */
+    public static Middleware link(Middleware first, Middleware... chain) {
+        Middleware head = first;
+        for (Middleware nextInChain : chain) {
+            head.next = nextInChain;
+            head = nextInChain;
+        }
+        return first;
+    }
+
+    /**
+     * Subclasses will implement this method with concrete checks.
+     */
+    public abstract boolean check(String email, String password);
+
+    /**
+     * Runs check on the next object in chain or ends traversing if we're in last object in chain.
+     */
+    protected boolean checkNext(String email, String password) {
+        if (next == null) {
+            return true;
+        }
+        return next.check(email, password);
+    }
+}
