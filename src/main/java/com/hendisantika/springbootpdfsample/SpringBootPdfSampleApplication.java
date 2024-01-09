7f2eacd4a4cb6609fb5e026fcca75de442ec6256
hendisantika <hendisantika@yahoo.co.id>
2024-01-09 08:25:29 +0700

    Create starter project 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/com/hendisantika/springbootpdfsample/SpringBootPdfSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootpdfsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPdfSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPdfSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootpdfsample/SpringBootPdfSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootpdfsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPdfSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
