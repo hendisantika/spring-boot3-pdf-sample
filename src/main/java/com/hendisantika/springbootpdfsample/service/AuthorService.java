9403a078c20ef9bd6ae5527f5bc2c541e6298c08
hendisantika <hendisantika@yahoo.co.id>
2024-01-09 08:56:19 +0700

    Add getById in AuthorService 🫸🌀✏️📗🐧🐳⬆

--- src/main/java/com/hendisantika/springbootpdfsample/service/AuthorService.java
+++ src/main/java/com/hendisantika/springbootpdfsample/service/AuthorService.java
@@ -1,10 +1,12 @@
 package com.hendisantika.springbootpdfsample.service;
 
 import com.hendisantika.springbootpdfsample.entity.Author;
+import com.hendisantika.springbootpdfsample.exception.DataNotFoundException;
 import com.hendisantika.springbootpdfsample.repository.AuthorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 /**
@@ -26,5 +28,14 @@ public class AuthorService {
     public List<Author> getAllAuthors() {
         List<Author> authorList = authorRepository.findAll();
         return authorList;
+    }
+
+    public Author getById(Long id) {
+        return authorRepository
+                .findById(id)
+                .orElseThrow(
+                        () ->
+                                new DataNotFoundException(
+                                        MessageFormat.format("Author id {0} not found", String.valueOf(id))));
     }
 }
