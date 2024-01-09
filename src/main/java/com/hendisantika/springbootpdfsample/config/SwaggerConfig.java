bba0056e4f7cc4c44cad7c9ddb58c41ba05a12d5
hendisantika <hendisantika@yahoo.co.id>
2024-01-09 08:37:51 +0700

    Add postApi in SwaggerConfig🫸🌀✏️📗🐧🐳⬆

--- src/main/java/com/hendisantika/springbootpdfsample/config/SwaggerConfig.java
+++ src/main/java/com/hendisantika/springbootpdfsample/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -48,5 +49,10 @@ public class SwaggerConfig implements WebMvcConfigurer {
                         new ExternalDocumentation()
                                 .description("Documentation")
                                 .url("https://s.id/hendisantika"));
+    }
+
+    @Bean
+    public GroupedOpenApi postApi() {
+        return groupedOpenApi("posts", "/v1/posts/**");
     }
 }
