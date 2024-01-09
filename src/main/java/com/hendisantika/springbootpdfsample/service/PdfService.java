ec427d10037be9cd98f24d901a6cd6f31e86f7c1
hendisantika <hendisantika@yahoo.co.id>
2024-01-09 08:57:10 +0700

    Add PdfService 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/com/hendisantika/springbootpdfsample/service/PdfService.java
@@ -0,0 +1,20 @@
+package com.hendisantika.springbootpdfsample.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-pdf-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/9/24
+ * Time: 08:56
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class PdfService {
+
+}
