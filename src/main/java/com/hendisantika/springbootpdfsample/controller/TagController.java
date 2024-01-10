e392f08a3141d1262e8f12a3ee7dbbf6aa46b14e
hendisantika <hendisantika@yahoo.co.id>
2024-01-10 07:33:13 +0700

    Modify TagController 🫸🌀✏️📗 :octocat:

--- src/main/java/com/hendisantika/springbootpdfsample/controller/TagController.java
+++ src/main/java/com/hendisantika/springbootpdfsample/controller/TagController.java
@@ -30,28 +30,28 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TagController {
 
-    private final TagService service;
+    private final TagService tagService;
 
     @GetMapping("/v1/tags")
     public ResponseEntity<List<Tag>> getAllTags() {
-        List<Tag> list = service.getAllTags();
+        List<Tag> list = tagService.getAllTags();
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @GetMapping("/v1/tags/{id}")
     public ResponseEntity<Tag> getTagById(@PathVariable("id") Long id) {
-        Tag entity = service.getById(id);
+        Tag entity = tagService.getById(id);
         return new ResponseEntity<>(entity, HttpStatus.OK);
     }
 
     @PostMapping("/v1/tags")
     public ResponseEntity<Tag> createOrUpdate(@RequestBody Tag Tag) {
-        Tag updated = service.createOrUpdate(Tag);
+        Tag updated = tagService.createOrUpdate(Tag);
         return new ResponseEntity<>(updated, HttpStatus.OK);
     }
 
     @DeleteMapping("/v1/tags/{id}")
     public void deleteById(@PathVariable("id") Long id) {
-        service.deleteById(id);
+        tagService.deleteById(id);
     }
 }
