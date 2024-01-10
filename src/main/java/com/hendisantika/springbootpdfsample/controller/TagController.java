f3ffcb014cec8acbb6add752ed4aab146f2f4e57
hendisantika <hendisantika@yahoo.co.id>
2024-01-10 07:31:53 +0700

    Add getAllTags in TagController 🫸🌀✏️📗 :octocat:

--- src/main/java/com/hendisantika/springbootpdfsample/controller/TagController.java
+++ src/main/java/com/hendisantika/springbootpdfsample/controller/TagController.java
@@ -1,9 +1,15 @@
 package com.hendisantika.springbootpdfsample.controller;
 
+import com.hendisantika.springbootpdfsample.entity.Tag;
 import com.hendisantika.springbootpdfsample.service.TagService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +27,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class TagController {
 
     private final TagService service;
+
+    @GetMapping("/v1/tags")
+    public ResponseEntity<List<Tag>> getAllTags() {
+        List<Tag> list = service.getAllTags();
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
 }
