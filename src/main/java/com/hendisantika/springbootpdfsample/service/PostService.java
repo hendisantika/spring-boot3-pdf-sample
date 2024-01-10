716efce6af1103dd9bdf93969b94a9f7f2098310
hendisantika <hendisantika@yahoo.co.id>
2024-01-10 07:20:07 +0700

    Add PostService 🫸🌀✏️📗 :octocat:

---
+++ src/main/java/com/hendisantika/springbootpdfsample/service/PostService.java
@@ -0,0 +1,33 @@
+package com.hendisantika.springbootpdfsample.service;
+
+import com.hendisantika.springbootpdfsample.repository.AuthorRepository;
+import com.hendisantika.springbootpdfsample.repository.PostRepository;
+import com.hendisantika.springbootpdfsample.repository.TagRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-pdf-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/10/24
+ * Time: 07:19
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class PostService {
+
+    private final ModelMapper modelMapper;
+
+    private final PostRepository postRepository;
+
+    private final AuthorRepository authorRepository;
+
+    private final TagRepository tagRepository;
+}
