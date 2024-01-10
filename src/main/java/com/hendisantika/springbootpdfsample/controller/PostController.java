fba20ae57c91430da0359c231ad0caec4d84c36e
hendisantika <hendisantika@yahoo.co.id>
2024-01-10 07:29:09 +0700

    Add createOrUpdate in PostController 🫸🌀✏️📗 :octocat:

--- src/main/java/com/hendisantika/springbootpdfsample/controller/PostController.java
+++ src/main/java/com/hendisantika/springbootpdfsample/controller/PostController.java
@@ -1,8 +1,10 @@
 package com.hendisantika.springbootpdfsample.controller;
 
+import com.hendisantika.springbootpdfsample.dto.PostDTO;
 import com.hendisantika.springbootpdfsample.entity.Post;
 import com.hendisantika.springbootpdfsample.service.PostService;
 import com.hendisantika.springbootpdfsample.util.PageUtils;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,5 +47,11 @@ public class PostController {
     public ResponseEntity<Post> getPostById(@PathVariable("id") Long id) {
         Post entity = service.getById(id);
         return new ResponseEntity<>(entity, HttpStatus.OK);
+    }
+
+    @PostMapping("/v1/posts")
+    public ResponseEntity<Post> createOrUpdate(@Valid @RequestBody PostDTO postDTO) {
+        Post updated = service.createOrUpdate(postDTO);
+        return new ResponseEntity<>(updated, HttpStatus.OK);
     }
 }
