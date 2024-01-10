4779583ff2210244e8d45add3f0b19b32aae07c6
hendisantika <hendisantika@yahoo.co.id>
2024-01-10 07:34:49 +0700

    Modify AuthorController 🫸🌀✏️📗 :octocat:

--- src/main/java/com/hendisantika/springbootpdfsample/controller/AuthorController.java
+++ src/main/java/com/hendisantika/springbootpdfsample/controller/AuthorController.java
@@ -30,28 +30,28 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AuthorController {
 
-    private final AuthorService service;
+    private final AuthorService authorService;
 
     @GetMapping("/v1/authors")
     public ResponseEntity<List<Author>> getAllAuthors() {
-        List<Author> list = service.getAllAuthors();
+        List<Author> list = authorService.getAllAuthors();
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @GetMapping("/v1/authors/{id}")
     public ResponseEntity<Author> getAuthorById(@PathVariable("id") Long id) {
-        Author entity = service.getById(id);
+        Author entity = authorService.getById(id);
         return new ResponseEntity<>(entity, HttpStatus.OK);
     }
 
     @PostMapping("/v1/authors")
     public ResponseEntity<Author> createOrUpdate(@RequestBody Author Author) {
-        Author updated = service.createOrUpdate(Author);
+        Author updated = authorService.createOrUpdate(Author);
         return new ResponseEntity<>(updated, HttpStatus.OK);
     }
 
     @DeleteMapping("/v1/authors/{id}")
     public void deleteById(@PathVariable("id") Long id) {
-        service.deleteById(id);
+        authorService.deleteById(id);
     }
 }
