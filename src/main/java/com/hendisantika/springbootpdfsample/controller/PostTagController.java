470474bb02b4b8493f7256a7549aa4fff7965063
hendisantika <hendisantika@yahoo.co.id>
2024-01-10 07:30:10 +0700

    Add getAllTagsByPostId in PostTagController 🫸🌀✏️📗 :octocat:

--- src/main/java/com/hendisantika/springbootpdfsample/controller/PostTagController.java
+++ src/main/java/com/hendisantika/springbootpdfsample/controller/PostTagController.java
@@ -1,9 +1,16 @@
 package com.hendisantika.springbootpdfsample.controller;
 
+import com.hendisantika.springbootpdfsample.entity.Tag;
 import com.hendisantika.springbootpdfsample.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +28,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostTagController {
 
     private final PostService service;
+
+    @GetMapping("/v1/posts/{id}/tags")
+    public ResponseEntity<List<Tag>> getAllTagsByPostId(@PathVariable(value = "id") Long id) {
+        List<Tag> tagList = service.getAllTagsByPostId(id);
+        return new ResponseEntity<>(tagList, HttpStatus.OK);
+    }
 }
